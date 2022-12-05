a2ad3b1dd1e725e44aae1c907335b9b0b7da7856
Luis Felipe <lipe.arraial1995@gmail.com>
2022-12-05 14:35:45 -0300

    textField e DatePicker

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -73,7 +73,7 @@ public class SellerDaoJDBC implements SellerDao {
 					+ "WHERE Id = ?");
 			
 			st.setString(1, obj.getName());
-			st.setString(2, obj.getEmail());
+			st.setString(2, obj.getEmail());		
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
@@ -142,7 +142,7 @@ public class SellerDaoJDBC implements SellerDao {
 		obj.setName(rs.getString("Name"));
 		obj.setEmail(rs.getString("Email"));
 		obj.setBaseSalary(rs.getDouble("BaseSalary"));
-		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setBirthDate(new java.util.Date(rs.getTimestamp("BirthDate").getTime()));
 		obj.setDepartment(dep);
 		return obj;
 	}
